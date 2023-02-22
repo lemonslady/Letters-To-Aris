537d8ecbf227c7e94822bcc0160a90025e8f3a91
lemonslady <113531950+lemonslady@users.noreply.github.com>
2023-02-22 11:45:56 +0100

    Initial Commit

---
+++ Escape/EscapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EscapeApp.swift
+//  Escape
+//
+//  Created by Giulia Casucci on 22/02/23.
+//
+
+import SwiftUI
+
+@main
+struct EscapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
